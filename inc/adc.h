511b0f9cd73d1731a62ef1de90142eafa502a0e7
Peter Mamonov <pmamonov@gmail.com>
2017-07-19 01:02:09 +0300

    adc: add support for channels selection in runtime

--- inc/adc.h
+++ inc/adc.h
@@ -1,26 +1,16 @@
-#ifndef __adc_h
-#define __adc_h
+#ifndef __ADC_H__
+#define __ADC_H__
 
 #include "stm32f10x_gpio.h"
 #include "FreeRTOS.h"
 //#include "task.h"
 #include "semphr.h"
 
-#define ADC_NCHAN 64
-#define ADC_MUX_NCHAN 8
-#define ADC_MUX_NPINS 4
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
-#define ADC_MUX_RCC RCC_APB2Periph_GPIOC
-#define ADC_MUX_GPIO GPIOC
-#define ADC_MUX_PINS {GPIO_Pin_6,GPIO_Pin_7,GPIO_Pin_8,GPIO_Pin_9}
-
-//void adc_init(void);
-
-//int adc_get(int i);
-
+void adc_init(int);
+int adc_get(int);
+int adc_get_stored(int);
 void vADCTask(void* vpars);
 
-extern int adc_vals[ADC_NCHAN];
-extern xSemaphoreHandle adc_vals_lock;
-
-#endif
+#endif /* __ADC_H__ */
--- src/adc.c
+++ src/adc.c
@@ -2,43 +2,125 @@
 #include "task.h"
 #include "adc.h"
 
-int adc_vals[ADC_NCHAN];
+struct adc_chan {
+	uint32_t	rcc;
+	GPIO_TypeDef 	*gpio;
+	uint16_t	pin;
+};
 
-uint16_t adc_mux_pins[] = ADC_MUX_PINS;
+static struct adc_chan chan_list[] = {
+	{	/* channel 0 */
+		.rcc = 	RCC_APB2Periph_GPIOA,
+		.gpio =	GPIOA,
+		.pin =	GPIO_Pin_0,
+	},
+	{	/* channel 1 */
+		.rcc = 	RCC_APB2Periph_GPIOA,
+		.gpio =	GPIOA,
+		.pin =	GPIO_Pin_1,
+	},
+	{	/* channel 2 */
+		.rcc = 	RCC_APB2Periph_GPIOA,
+		.gpio =	GPIOA,
+		.pin =	GPIO_Pin_2,
+	},
+	{	/* channel 3 */
+		.rcc = 	RCC_APB2Periph_GPIOA,
+		.gpio =	GPIOA,
+		.pin =	GPIO_Pin_3,
+	},
+	{	/* channel 4 */
+		.rcc = 	RCC_APB2Periph_GPIOA,
+		.gpio =	GPIOA,
+		.pin =	GPIO_Pin_4,
+	},
+	{	/* channel 5 */
+		.rcc = 	RCC_APB2Periph_GPIOA,
+		.gpio =	GPIOA,
+		.pin =	GPIO_Pin_5,
+	},
+	{	/* channel 6 */
+		.rcc = 	RCC_APB2Periph_GPIOA,
+		.gpio =	GPIOA,
+		.pin =	GPIO_Pin_6,
+	},
+	{	/* channel 7 */
+		.rcc = 	RCC_APB2Periph_GPIOA,
+		.gpio =	GPIOA,
+		.pin =	GPIO_Pin_7,
+	},
+	{	/* channel 8 */
+		.rcc = 	RCC_APB2Periph_GPIOB,
+		.gpio =	GPIOB,
+		.pin =	GPIO_Pin_0,
+	},
+	{	/* channel 9 */
+		.rcc = 	RCC_APB2Periph_GPIOB,
+		.gpio =	GPIOB,
+		.pin =	GPIO_Pin_1,
+	},
+	{	/* channel 10 */
+		.rcc = 	RCC_APB2Periph_GPIOC,
+		.gpio =	GPIOC,
+		.pin =	GPIO_Pin_0,
+	},
+	{	/* channel 11 */
+		.rcc = 	RCC_APB2Periph_GPIOC,
+		.gpio =	GPIOC,
+		.pin =	GPIO_Pin_1,
+	},
+	{	/* channel 12 */
+		.rcc = 	RCC_APB2Periph_GPIOC,
+		.gpio =	GPIOC,
+		.pin =	GPIO_Pin_2,
+	},
+	{	/* channel 13 */
+		.rcc = 	RCC_APB2Periph_GPIOC,
+		.gpio =	GPIOC,
+		.pin =	GPIO_Pin_3,
+	},
+	{	/* channel 14 */
+		.rcc = 	RCC_APB2Periph_GPIOC,
+		.gpio =	GPIOC,
+		.pin =	GPIO_Pin_4,
+	},
+	{	/* channel 15 */
+		.rcc = 	RCC_APB2Periph_GPIOC,
+		.gpio =	GPIOC,
+		.pin =	GPIO_Pin_5,
+	},
+};
+
+int adc_vals[ARRAY_SIZE(chan_list)];
 
 xSemaphoreHandle adc_vals_lock = NULL;
 
-void adc_init(void)
+void adc_init(int chans)
 {
 	int i;
-
+	uint32_t tmp;
 	GPIO_InitTypeDef sGPIOinit;
 	ADC_InitTypeDef sADCinit;
+
 	/* set  adc clock to 48/4=12MHz */
 	RCC_ADCCLKConfig(RCC_PCLK2_Div4);
+
 	/* enable  ADC and input GPIOs clock */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA |
-			       RCC_APB2Periph_GPIOB |
-			       RCC_APB2Periph_GPIOC |
-			       RCC_APB2Periph_ADC1,
-			       ENABLE);
-	/* configure all 16 adc inputs */
+	tmp = RCC_APB2Periph_ADC1;
+	for (i = 0; i < ARRAY_SIZE(chan_list); i++)
+		if (chans & (1 << i))
+			tmp |= chan_list[i].rcc;
+	RCC_APB2PeriphClockCmd(tmp, ENABLE);
+
+	/* configure ADC inputs */
 	sGPIOinit.GPIO_Speed = GPIO_Speed_10MHz;
 	sGPIOinit.GPIO_Mode = GPIO_Mode_AIN;
+	for (i = 0; i < ARRAY_SIZE(chan_list); i++)
+		if (chans & (1 << i)) {
+			sGPIOinit.GPIO_Pin = chan_list[i].pin;
+			GPIO_Init(chan_list[i].gpio, &sGPIOinit);
+		}
 
-	sGPIOinit.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | 
-			     GPIO_Pin_2 | GPIO_Pin_3 |
-			     GPIO_Pin_4 | GPIO_Pin_5 |
-			     GPIO_Pin_6 | GPIO_Pin_7;
-	GPIO_Init(GPIOA, &sGPIOinit);
-
-	sGPIOinit.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 |
-			     GPIO_Pin_2 | GPIO_Pin_3 |
-			     GPIO_Pin_4 | GPIO_Pin_5;
-	GPIO_Init(GPIOC, &sGPIOinit);
-
-	sGPIOinit.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
-	GPIO_Init(GPIOB, &sGPIOinit);
 	/* adc setup */
 	sADCinit.ADC_Mode = ADC_Mode_Independent;
 	sADCinit.ADC_ScanConvMode = DISABLE;
@@ -46,70 +128,38 @@ void adc_init(void)
 	sADCinit.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
 	sADCinit.ADC_DataAlign = ADC_DataAlign_Right;
 	sADCinit.ADC_NbrOfChannel = 1;
-
 	ADC_Init(ADC1,&sADCinit);
 
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1,
-				 ADC_SampleTime_7Cycles5);
-
 	ADC_Cmd(ADC1,ENABLE);
+
 	ADC_StartCalibration(ADC1);
-	while (ADC_GetCalibrationStatus(ADC1) != SET);
-
-	/* external multiplexer control pins setup */
-	RCC_APB2PeriphClockCmd(ADC_MUX_RCC, ENABLE);
-
-	sGPIOinit.GPIO_Speed = GPIO_Speed_10MHz;
-	sGPIOinit.GPIO_Mode = GPIO_Mode_Out_OD;
-	sGPIOinit.GPIO_Pin = 0;
-	for (i = 0; i < ADC_MUX_NPINS; i++)
-		sGPIOinit.GPIO_Pin |= adc_mux_pins[i];
-	GPIO_Init(ADC_MUX_GPIO, &sGPIOinit);
-
-	return;
+	while (ADC_GetCalibrationStatus(ADC1) != SET)
+		;
 }
 
 int adc_get(int i)
 {
 	ADC_ClearFlag(ADC1, ADC_FLAG_EOC);
-	ADC_RegularChannelConfig(ADC1, i, 1, ADC_SampleTime_55Cycles5);
-	ADC_SoftwareStartConvCmd(ADC1,ENABLE);
-	while (!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));
+	ADC_RegularChannelConfig(ADC1, i, 1, ADC_SampleTime_71Cycles5);
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+	while (!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC))
+		;
 	return ADC_GetConversionValue(ADC1);
 }
 
-void adc_switch_mux(int n)
+int adc_get_stored(int i)
 {
-	volatile int i;
+	if (i >= ARRAY_SIZE(chan_list))
+		return 0;
 
-	for (i = 0; i < ADC_MUX_NPINS; i++) {
-		if ((1 << i) & n)
-			GPIO_SetBits(ADC_MUX_GPIO, adc_mux_pins[i]);
-		else
-			GPIO_ResetBits(ADC_MUX_GPIO, adc_mux_pins[i]);
-	}
-
-	for (i = 500; i; i--);
-}
+	return adc_vals[i];
+};
 
 void vADCTask(void* vpars)
 {
 	int i, j, ref;
 
-	adc_init();
-	adc_vals_lock = xSemaphoreCreateMutex();
-
 	while (1) {
-		xSemaphoreTake(adc_vals_lock, portMAX_DELAY);
-		for (i = 0; i < ADC_NCHAN / ADC_MUX_NCHAN; i++) {
-			ref = adc_get(ADC_NCHAN / ADC_MUX_NCHAN + i);
-			for (j = 0; j < ADC_MUX_NCHAN; j++) {
-				adc_switch_mux(j);
-				//TODO: delay?
-				adc_vals[i*ADC_MUX_NCHAN+j] = adc_get(i)-ref;
-			}
-		}
-		xSemaphoreGive(adc_vals_lock);
 		vTaskDelay(100);
 	}
 }
