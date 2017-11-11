f8cd3e5aed2fb1efea2a325abfb85c6effb35ac2
Peter Mamonov <pmamonov@gmail.com>
2017-11-12 02:13:41 +0300

    fixup! fixup! adc: add separate calibration and counter for xenon flow

--- cal_xe.sh
+++ cal_xe.sh
@@ -4,19 +4,24 @@ TTY='/dev/ttyACM0'
 
 function adc_cal()
 {
-	printf 'cal %d %d %d\r' $1 $2 $3 > $TTY
+	printf 'xcal %d %d %d\r' $1 $2 $3 > $TTY
 	sleep 0.1
 }
 
 adc_cal 0 620 0
 adc_cal 1 1974 3281
 adc_cal 2 2595 6874
-adc_cal 3 2855 10312
-adc_cal 4 3091 13749
-adc_cal 5 4000 13749
+adc_cal 3 2855 11088
+adc_cal 4 3091 14784
+adc_cal 5 4000 14784
 adc_cal 6 0 0
 adc_cal 7 0 0
 adc_cal 8 0 0
 adc_cal 9 0 0
 
 # printf 'cal_save\r' > $TTY
+
+# f=366 v=123 vr=110
+# f=540 v=132 vr=145
+# f=580 v=147 vr=155
+# f=610 v=132 vr=150
