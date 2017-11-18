51f3ac9b78a583c60932f6fb1c2074ac9b4d0611
Peter Mamonov <pmamonov@gmail.com>
2017-11-19 01:32:54 +0300

    i2c_lcd: don't hang, return error on timeout

--- i2c_lcd/I2C.h
+++ i2c_lcd/I2C.h
@@ -1,10 +1,22 @@
+#ifndef __I2C_H__
+#define __I2C_H__
+
 #include "stm32f10x.h"
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_i2c.h"
 
+enum i2c_err {
+    EBUSY = 1,
+    EMASTER,
+    ETRANS,
+    ERECV,
+    EMBTRANS,
+};
+
 void init_I2C1(void);
-void I2C_StartTransmission(I2C_TypeDef* I2Cx,
+int I2C_StartTransmission(I2C_TypeDef* I2Cx,
 			   uint8_t transmissionDirection,
 			   uint8_t slaveAddress);
-void I2C_WriteData(I2C_TypeDef* I2Cx, uint8_t data);
+int I2C_WriteData(I2C_TypeDef* I2Cx, uint8_t data);
+#endif /*__I2C_H__*/
--- i2c_lcd/LiquidCrystal_I2C.c
+++ i2c_lcd/LiquidCrystal_I2C.c
@@ -16,8 +16,8 @@
 //Library version:1.1
 
 
-void LCDI2C_write(uint8_t value){
-	LCDI2C_send(value, Rs);
+int LCDI2C_write(uint8_t value){
+	return LCDI2C_send(value, Rs);
 }
 
 
@@ -43,7 +43,7 @@ void LCDI2C_write(uint8_t value){
 
 LiquidCrystal_I2C_Def lcdi2c;
 
-void LCDI2C_init(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows)
+int LCDI2C_init(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows)
 {
   lcdi2c.Addr = lcd_Addr;
   lcdi2c.cols = lcd_cols;
@@ -52,10 +52,12 @@ void LCDI2C_init(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows)
 
   init_I2C1(); // Wire.begin();
   lcdi2c.displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
-  LCDI2C_begin(lcd_cols, lcd_rows);
+  return LCDI2C_begin(lcd_cols, lcd_rows);
 }
 
-void LCDI2C_begin(uint8_t cols, uint8_t lines) {//, uint8_t dotsize) {
+int LCDI2C_begin(uint8_t cols, uint8_t lines) {//, uint8_t dotsize) {
+	int ret;
+
 	if (lines > 1) {
 		lcdi2c.displayfunction |= LCD_2LINE;
 	}
@@ -72,7 +74,9 @@ void LCDI2C_begin(uint8_t cols, uint8_t lines) {//, uint8_t dotsize) {
 	vTaskDelay(50);
 
 	// Now we pull both RS and R/W low to begin commands
-	LCDI2C_expanderWrite(lcdi2c.backlightval);	// reset expanderand turn backlight off (Bit 8 =1)
+	ret = LCDI2C_expanderWrite(lcdi2c.backlightval);	// reset expanderand turn backlight off (Bit 8 =1)
+	if (ret)
+		return ret;
 	vTaskDelay(1000);
 
   	//put the LCD into 4 bit mode
@@ -80,58 +84,79 @@ void LCDI2C_begin(uint8_t cols, uint8_t lines) {//, uint8_t dotsize) {
 	// figure 24, pg 46
 
 	  // we start in 8bit mode, try to set 4 bit mode
-   LCDI2C_write4bits(0x03 << 4);
-   vTaskDelay(5); // wait min 4.1ms
+    ret = LCDI2C_write4bits(0x03 << 4);
+    if (ret)
+    	return ret;
+    vTaskDelay(5); // wait min 4.1ms
 
    // second try
-   LCDI2C_write4bits(0x03 << 4);
-   vTaskDelay(5); // wait min 4.1ms
+    ret = LCDI2C_write4bits(0x03 << 4);
+    if (ret)
+    	return ret;
+    vTaskDelay(5); // wait min 4.1ms
 
    // third go!
-   LCDI2C_write4bits(0x03 << 4);
-   vTaskDelay(1);
+    ret = LCDI2C_write4bits(0x03 << 4);
+    if (ret)
+    	return ret;
+    vTaskDelay(1);
 
    // finally, set to 4-bit interface
-   LCDI2C_write4bits(0x02 << 4);
+    ret |= LCDI2C_write4bits(0x02 << 4);
+    if (ret)
+    	return ret;
 
 
 	// set # lines, font size, etc.
-	LCDI2C_command(LCD_FUNCTIONSET | lcdi2c.displayfunction);
+	ret = LCDI2C_command(LCD_FUNCTIONSET | lcdi2c.displayfunction);
+    if (ret)
+    	return ret;
 
 	// turn the display on with no cursor or blinking default
 	lcdi2c.displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
-	LCDI2C_display();
+	ret = LCDI2C_display();
+    if (ret)
+    	return ret;
 
 	// clear it off
-	LCDI2C_clear();
+	ret = LCDI2C_clear();
+    if (ret)
+    	return ret;
 
 	// Initialize to default text direction (for roman languages)
 	lcdi2c.displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
 
 	// set the entry mode
-	LCDI2C_command(LCD_ENTRYMODESET | lcdi2c.displaymode);
+	ret = LCDI2C_command(LCD_ENTRYMODESET | lcdi2c.displaymode);
+    if (ret)
+    	return ret;
 
-	LCDI2C_home();
-
+	ret = LCDI2C_home();
+	
+	return ret;
 }
 
 /********** high level commands, for the user! */
-void LCDI2C_clear(){
-	LCDI2C_command(LCD_CLEARDISPLAY);// clear display, set cursor position to zero
+int LCDI2C_clear(){
+	int ret;
+	ret = LCDI2C_command(LCD_CLEARDISPLAY);// clear display, set cursor position to zero
 	vTaskDelay(3);  // this command takes a long time!
+    return ret;
 }
 
-void LCDI2C_home(){
-	LCDI2C_command(LCD_RETURNHOME);  // set cursor position to zero
+int LCDI2C_home(){
+    int ret = LCDI2C_command(LCD_RETURNHOME);  // set cursor position to zero
+
 	vTaskDelay(3);  // this command takes a long time!
+    return ret;
 }
 
-void LCDI2C_setCursor(uint8_t col, uint8_t row){
+int LCDI2C_setCursor(uint8_t col, uint8_t row){
 	int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
 	if ( row > lcdi2c.numlines ) {
 		row = lcdi2c.numlines-1;    // we count rows starting w/0
 	}
-	LCDI2C_command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
+	return LCDI2C_command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
 }
 
 // Turn the display on/off (quickly)
@@ -140,9 +165,9 @@ void LCDI2C_noDisplay() {
 	LCDI2C_command(LCD_DISPLAYCONTROL | lcdi2c.displaycontrol);
 }
 
-void LCDI2C_display() {
+int LCDI2C_display() {
 	lcdi2c.displaycontrol |= LCD_DISPLAYON;
-	LCDI2C_command(LCD_DISPLAYCONTROL | lcdi2c.displaycontrol);
+	return LCDI2C_command(LCD_DISPLAYCONTROL | lcdi2c.displaycontrol);
 }
 
 // Turns the underline cursor on/off
@@ -210,44 +235,55 @@ void LCDI2C_createChar(uint8_t location, uint8_t charmap[]) {
 }
 
 // Turn the (optional) backlight off/on
-void LCDI2C_noBacklight(void) {
+int LCDI2C_noBacklight(void) {
 	lcdi2c.backlightval=LCD_NOBACKLIGHT;
-	LCDI2C_expanderWrite(0);
+	return LCDI2C_expanderWrite(0);
 }
 
-void LCDI2C_backlight(void) {
+int LCDI2C_backlight(void) {
 	lcdi2c.backlightval=LCD_BACKLIGHT;
-	LCDI2C_expanderWrite(0);
+	return LCDI2C_expanderWrite(0);
 }
 
 
 
 /*********** mid level commands, for sending data/cmds */
 
-void LCDI2C_command(uint8_t value) {
-	LCDI2C_send(value, 0);
+int LCDI2C_command(uint8_t value) {
+	return LCDI2C_send(value, 0);
 }
 
 
 /************ low level data pushing commands **********/
 
 // write either command or data
-void LCDI2C_send(uint8_t value, uint8_t mode) {
+int LCDI2C_send(uint8_t value, uint8_t mode) {
 	uint8_t highnib=value&0xf0;
 	uint8_t lownib=(value<<4)&0xf0;
-       LCDI2C_write4bits((highnib)|mode);
-	LCDI2C_write4bits((lownib)|mode);
+    int ret = LCDI2C_write4bits((highnib)|mode);
+	if (ret)
+		return ret;
+	return LCDI2C_write4bits((lownib)|mode);
 }
 
-void LCDI2C_write4bits(uint8_t value) {
-	LCDI2C_expanderWrite(value);
-	LCDI2C_pulseEnable(value);
+int LCDI2C_write4bits(uint8_t value) {
+	int ret = LCDI2C_expanderWrite(value);
+
+	if (ret)
+		return ret;
+	return LCDI2C_pulseEnable(value);
 }
 
-void LCDI2C_expanderWrite(uint8_t _data){
-	I2C_StartTransmission (I2C1, I2C_Direction_Transmitter, lcdi2c.Addr); //Wire.beginTransmission(_Addr);
-	I2C_WriteData(I2C1, (int)(_data) | lcdi2c.backlightval);  //printIIC((int)(_data) | _backlightval);
+int LCDI2C_expanderWrite(uint8_t _data){
+	int ret = I2C_StartTransmission (I2C1,
+					 I2C_Direction_Transmitter,
+					 lcdi2c.Addr);
+	if (ret)
+		return ret;
+	ret = I2C_WriteData(I2C1, (int)(_data) | lcdi2c.backlightval);
 	I2C_GenerateSTOP(I2C1, ENABLE); //Wire.endTransmission();
+
+	return ret;
 }
 
 static void __delay(int n)
@@ -258,12 +294,19 @@ static void __delay(int n)
 		;	
 }
 
-void LCDI2C_pulseEnable(uint8_t _data){
-	LCDI2C_expanderWrite(_data | En);	// En high
+int LCDI2C_pulseEnable(uint8_t _data){
+	int ret;
+
+	ret = LCDI2C_expanderWrite(_data | En);	// En high
+	if (ret)
+		return ret;
 	__delay(2);		// enable pulse must be >450ns
 
-	LCDI2C_expanderWrite(_data & ~En);	// En low
+	ret = LCDI2C_expanderWrite(_data & ~En);	// En low
+	if (ret)
+		return ret;
 	__delay(40);		// commands need > 37us to settle
+	return ret;
 }
 
 
@@ -298,11 +341,15 @@ void LCDI2C_setBacklight(uint8_t new_val){
 }
 
 //Функция передачи строки через USART
-void LCDI2C_write_String(char* str) {
+int LCDI2C_write_String(char* str) {
   uint8_t i=0;
+  int ret;
+
   while(str[i])
   {
-    LCDI2C_write(str[i]);
+    ret = LCDI2C_write(str[i]);
+    if (ret)
+    	return ret;
     i++;
   }
 }
