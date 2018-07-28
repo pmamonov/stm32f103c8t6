0cd18851f0061ac45ea857067f25c262e1d13df5
Peter Mamonov <pmamonov@gmail.com>
2018-07-29 02:58:56 +0300

    add aquarium.py

---
+++ aquarium.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python2.7
+
+import sys
+from serial import Serial
+from time import sleep
+
+class aquarium:
+	s = None
+
+	def __init__(self, tty="/dev/ttyACM0"):
+		self.tty = tty
+		self.init()
+
+	def init(self):
+		if self.s:
+			self.s.close()
+		self.s = Serial(port=self.tty, timeout=5)
+		self.s.write("adc_init\r")
+		self.s.read(1024)
+
+	
+	def adc(self, i):
+		while 1:
+			self.s.write("adc %d\r")
+			self.s.readline()
+			l = self.s.readline()
+			try:
+				s = l.split()[1]
+				return int(s, 16)
+			except ValueError:
+				print >>sys.stderr, "W: `%s`" % l.strip()
+				continue
+			except:
+				raise NameError, "`%s`" % l
+		
+if __name__ == "__main__":
+	a = aquarium()
+	while 1:
+		print "0x%x" % a.adc(0)
+		sys.stdout.flush()
+		sleep(1)
