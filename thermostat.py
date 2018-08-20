1d4d39b1ca99bc3a57e0f0cd5dc6fcf2a8332b57
zoidberg <zoidberg@aquarium.ERG>
2018-08-20 09:11:47 +0300

    Some bug were fixed

--- thermostat.py
+++ thermostat.py
@@ -3,44 +3,7 @@
 import sys,os
 from pylab import *
 from time import sleep,time,ctime
-from threading import Thread
-from serial import Serial
-
-class aquarium:
-  s = None
-  def __init__(self, tty="/dev/ttyACM0"):
-    self.tty = tty
-    self.init()
-
-  def init(self):
-    if self.s:
-      self.s.close()
-    self.s = Serial(port=self.tty, timeout=5)
-    self.s.write("adc_init 0 5\r")
-    self.s.read(1024)
-
-  # i equals the number of ADC channel: 0 -- 1, ... 5 -- 
-  def adc(self, i):
-    while 1:
-      self.s.write("adc %d 0\r" % i)
-      self.s.readline()
-      l = self.s.readline()
-      try:
-        s = l.split()[1]
-        return int(s, 16)
-      except ValueError:
-        print >>sys.stderr, "W: `%s`" % l.strip()
-        continue
-      except:
-        raise NameError, "`%s`" % l
-
-  # i equals the number of the STM output pin: 0 -- B10, 1 -- B11, 2 -- B12, 3 -- B13, 4 -- B14, and 5 -- B15
-  # v is a signal, 1 -- swith on, 0 -- switch off
-  # timeout is a switch-on time interval in ms
-  def gpio(self, i, v, timeout=1000):
-    self.s.write("gpio %d %d %d\r" % (i, v, timeout))
-    self.s.readline()
-    self.s.readline()
+from aquarium import *
 
 # function that returns the temperature according to the calibration coefficients stored in 'C'
 def tenz2temp(n,U):
@@ -56,21 +19,21 @@ def tenz2temp(n,U):
     C = [3.28016632e+06, 1.08619679e+04, 3.83385365e+00, 2.59776707e+02]
   elif n == 5:
     C = [3.27996990e+06, 1.08658416e+04, 3.83256133e+00, 2.59864404e+02]
-  return (C[0]+C[1]*U)/(C[2]*(3298.0-U))-C[3]
+  return (C[0]+C[1]*U)/(C[2]*(V-U))-C[3]
 
 # reference temperature: empty, N1, N2, N3, W, L -- THIS MUST BE CLARIFIED!!!
 def reference():
   day    = int(ctime().split()[2])
-  mounth = ctime().split()[1]
-  rT[1] = float(N1[((N1[:,0] == str(day))*(N1[:,1] == mounth)).nonzero()[0][0],2])
-  rT[2] = float(N2[((N2[:,0] == str(day))*(N2[:,1] == mounth)).nonzero()[0][0],2])
-  rT[3] = float(N3[((N3[:,0] == str(day))*(N3[:,1] == mounth)).nonzero()[0][0],2])
-  rT[4] = float(W[((W[:,0] == str(day))*(W[:,1] == mounth)).nonzero()[0][0],2])
-  rT[5] = float(L[((L[:,0] == str(day))*(L[:,1] == mounth)).nonzero()[0][0],2])
+  month = ctime().split()[1]
+  rT[1] = float(N1[((N1[:,0] == str(day))*(N1[:,1] == month)).nonzero()[0][0],2])
+  rT[2] = float(N2[((N2[:,0] == str(day))*(N2[:,1] == month)).nonzero()[0][0],2])
+  rT[3] = float(N3[((N3[:,0] == str(day))*(N3[:,1] == month)).nonzero()[0][0],2])
+  rT[4] = float(W[((W[:,0] == str(day))*(W[:,1] == month)).nonzero()[0][0],2])
+  rT[5] = float(L[((L[:,0] == str(day))*(L[:,1] == month)).nonzero()[0][0],2])
   
 # function decides what power should be supplied to each channel
 def power():
-  cT = array(T)[-int(PDR*60/prd):,:].mean(0) # current temperature
+  cT = array(T)[-int(PRD*60/prd):,:].mean(0) # current temperature
   WTA.write(ctime()+'\t %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f\n' % tuple(cT))
   WTA.flush()
   print ctime()
@@ -81,12 +44,11 @@ def power():
   # it tryes to reach the target temperature T in one hour
   for p in range(6):
     PW[p] = (3600.0*PW[p] + C[p]*(dT1[p]-dT0[p]))/3600.0  # power in W
-    PW[p] = PW[p]*(PW[p] <= 120) + 120*(PW[p] > 120) # if the requested power exceeds the capacity of the heater (120W)
+    PW[p] = PW[p] if PW[p] <= 120 else 120 # if the requested power exceeds the capacity of the heater (120W)
     PW[p] = PW[p]*(PW[p] > 0) # if the power is negative, that is usual if it cools down too slow
   WP.write(ctime()+'\t %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n' % tuple(PW))
   WP.flush()
-  print ' '
-  print 'heat power:  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f' % tuple(PW)
+  print ' heat power: %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f' % tuple(PW)
 
 
 #########################################
@@ -98,6 +60,7 @@ def power():
 # main settings!!!!
 prd = 5.0  # period (sec) of the temperature measuring and the heaters switching ON
 PRD = 20   # period (min) of the derivatives calculation and heat power decision making
+V = 3298.0 # the reference potential for ADC 
 # common variables definition
 AQ = aquarium()
 T = []                                     # actual temperature
@@ -133,7 +96,7 @@ if os.path.isfile(nameP+'.log'):
   lines = F.readlines()
   F.close()
   PW = []
-  if len(lines) > 0:
+  if len(lines) > 1:
     for p in range(0,12*(len(lines) >= 12 + len(lines)*(len(lines) < 12))):
       PW.append(float32(lines[-p].split()[5:]))
     PW = array(PW).mean(0)
@@ -164,8 +127,8 @@ c = 0
 while True:
   temp = []
   for p in range(6):
-    V = (2*3298.0/(2**5))*(AQ.adc(p) - int('800000', 16))/int('ffffff', 16)
-    temp.append(tenz2temp(p,V))
+    v = (2*V/(2**5))*(AQ.adc(p) - int('800000', 16))/int('ffffff', 16)
+    temp.append(tenz2temp(p,v))
     sleep(0.2)
   T.append(temp) 
   WTF.write(ctime()+'\t')
@@ -177,9 +140,9 @@ while True:
       sleep(0.1)
   # this will chop the head of the temperature list
   if len(T) == 10000:
-    T[:5000] = [] 
+      T = T[5000:]
   # power definition each 10 minutes
-  if (c%120 == 0) and (c != 0):
+  if (c%int(PRD*60/prd) == 0) and (c != 0):
     power()
   # reference update
   tm = int16(ctime().split()[3].split(':'))
