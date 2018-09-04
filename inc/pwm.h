08d6be11c7edfb92a5c70cbbd5acb0ff9ca8a77e
Peter Mamonov <pmamonov@gmail.com>
2018-09-05 01:11:58 +0300

    fixup! add support for PWM

--- inc/pwm.h
+++ inc/pwm.h
@@ -1,14 +1,9 @@
 #ifndef __PWM_H__
 #define __PWM_H__
 
-enum pwm_id {
-	PWM0,
-	PWM1,
-	PWM2,
-	NPWM,
-};
-
-int pwm_init(enum pwm_id id, unsigned int hz);
-int pwm_set(enum pwm_id id, unsigned int dc);
+int pwm_count();
+int pwm_init(int id, unsigned int hz);
+int pwm_init_all(unsigned int hz);
+int pwm_set(int id, unsigned int dc);
 
 #endif /* __PWM_H__ */
