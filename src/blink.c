fb05b0fa8c84ee3131d65a6792d7c1bc28bfa160
Peter Mamonov <pmamonov@gmail.com>
2018-11-17 22:41:01 +0300

    blink: implement soft pwm for smooth blinking

--- src/blink.c
+++ src/blink.c
@@ -19,11 +19,34 @@ void blink_toggle() {
 		GPIO_SetBits(SYS_LED_GPIO, SYS_LED_PIN);
 }
 
+static void pwm1(int p, int d)
+{
+	if (d) {
+		GPIO_SetBits(SYS_LED_GPIO, SYS_LED_PIN);
+		vTaskDelay(d);
+	}
+	if (p - d) {
+		GPIO_ResetBits(SYS_LED_GPIO, SYS_LED_PIN);
+		vTaskDelay(p - d);
+	}
+}
+
+static void npwm(int n, int p, int d)
+{
+	while (n--)
+		pwm1(p, d);
+}
+
+#define P	10
 void vBlinkTask(void *vpars)
 {
+	int i;
+
 	blink_init();
 	while (1) {
-		blink_toggle();
-		vTaskDelay(200);
+		for (i = 0; i < P; i++)
+			npwm(10, P, i);
+		for (i = 0; i < P; i++)
+			npwm(10, P, P-i);
 	}
 }
